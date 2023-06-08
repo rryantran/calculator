ba6e05e46a6987bd80f5d3a6da39f8ac00f52112
ryantran <113406848+rryantran@users.noreply.github.com>
2023-06-07 17:13:53 -1000

    add calculator logic

---
+++ script.js
@@ -0,0 +1,28 @@
+// operations
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
+    default:
+      null;
+  }
+}
