d3735387888fdb266c8f5cadea0921c9f928bda1
ryantran <113406848+rryantran@users.noreply.github.com>
2023-06-08 22:01:39 -0500

    add basic functionality

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
+  if (b == 0) {
+    return "error";
+  }
   return a / b;
 }
 
 function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
@@ -26,3 +31,59 @@ function operate(operator, a, b) {
       null;
   }
 }
+
+function display(num) {
+  mainDisplay.textContent = num;
+}
+
+function clearDisplay() {
+  subDisplay.textContent = "";
+  mainDisplay.textContent = "";
+}
+
+function storeHistory(num) {
+  firstNum = num;
+}
+
+function cleaHistory() {
+  firstNum = "";
+  operator = "";
+  secondNum = "";
+}
+
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+const operators = ["+", "-", "x", "÷"];
+
+const mainDisplay = document.getElementById("main");
+const subDisplay = document.getElementById("sub");
+
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (this.className == "math operator" && firstNum) {
+      operator = this.textContent;
+      display();
+      console.log(firstNum, operator, secondNum);
+    } else if (this.className == "math num" && !operator) {
+      firstNum += this.textContent;
+      display(firstNum);
+      console.log(firstNum, operator, secondNum);
+    } else if (this.className == "math num" && operator) {
+      secondNum += this.textContent;
+      display(secondNum);
+      console.log(firstNum, operator, secondNum);
+    } else if (this.className == "math equals") {
+      subDisplay.textContent = `${firstNum} ${operator} ${secondNum}`;
+      let solution = operate(operator, firstNum, secondNum);
+      display(solution);
+      storeHistory(solution);
+      operator = "";
+      secondNum = "";
+    } else if (this.className == "erase clear") {
+      cleaHistory();
+      clearDisplay();
+    }
+  });
+});
